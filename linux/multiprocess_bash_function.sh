73fa36d48e4bd58d77320af930a887d47d6aebb6
Antonio Stanziola <stanziola.antonio@gmail.com>
2021-08-03 11:27:18 +0100

    updated multiprocess bash

--- linux/multiprocess_bash_function.sh
+++ linux/multiprocess_bash_function.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-MAX_PROCESSES=16
+# Adapted from:
+# https://unix.stackexchange.com/a/436713
+
+MAX_PROCESSES=16  # Max. number of parallel processes
 
 subprocess() {
 	eval subpath="$1"
 	echo "${subpath}"
 }
 
-rootdir="$(pwd)"
 for d in */ ; do
-	((i=i%MAX_PROCESSES)); ((i++==0)) && wait
-	
-	fullpath="$rootdir/$d"
-	
-	subprocess "\${fullpath}" &
+	subprocess "\${d}" &
+
+    if [[ $(jobs -r -p | wc -l) -ge $MAX_PROCESSES ]]; then
+        # now there are $N jobs already running, so wait here for any job to be finished
+        wait -n
+    fi
 done
+
+# no more jobs to be started but wait for pending jobs
+wait
+
 
