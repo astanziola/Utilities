cca5a16dbee16450349229784863ae948d0c13c3
Antonio Stanziola <stanziola.antonio@gmail.com>
2021-12-07 18:27:26 +0000

    suppress output

--- linux/test_notebooks_imp.sh
+++ linux/test_notebooks_imp.sh
@@ -7,12 +7,12 @@ cd ~/privacy-exercise
 
 # Throw an error if fails, time the execution of the command
 start_time=$(date +%s)
-jupyter nbconvert --execute _test_Exercise.ipynb --to python || exit 1
+jupyter nbconvert --execute _test_Exercise.ipynb --to python 2> /dev/null || exit 1
 end_time=`date +%s`
 echo "Correctly executed notebook _test_Exercise.ipynb in $((end_time-start_time)) seconds"
 
 start_time=$(date +%s)
-jupyter nbconvert --execute _test_Competition_1.ipynb --to python || exit 1
+jupyter nbconvert --execute _test_Competition_1.ipynb --to python 2> /dev/null || exit 1
 end_time=`date +%s`
 echo "Correctly executed notebook _test_Competition_1.ipynb in $((end_time-start_time)) seconds"
 echo "-- All notebooks of the privacy exercise have been executed successfully"
@@ -30,7 +30,7 @@ cd ~/scalable-recognition-with-a-vocabulary-tree
 echo "Downloading data before measuring execution time"
 python cbir/download.py
 start_time=$(date +%s)
-jupyter nbconvert --execute '_test_Scalable Recognition with a Vocabulary Tree copy.ipynb' --to python || exit 1
+jupyter nbconvert --execute '_test_Scalable Recognition with a Vocabulary Tree copy.ipynb' --to python 2> /dev/null || exit 1
 end_time=`date +%s`
 echo "Correctly executed notebook _test_Scalable Recognition with a Vocabulary Tree copy.ipynb in $((end_time-start_time)) seconds"
 echo "-- All notebooks of the Scalable Recognition exercise have been executed successfully"
